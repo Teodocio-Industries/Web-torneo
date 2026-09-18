c005dfcde4d0a44a78ed356ea090348336b4ebea
Jeronimo Velez <jemove2411@gmail.com>
2026-09-18 15:51:57 -0500

    Corregir rutas de GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
@@ -9,7 +9,7 @@ import { ConfirmProvider } from './components/ConfirmDialog/ConfirmDialog.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <ToastProvider>
           <ConfirmProvider>
@@ -17,6 +17,6 @@ createRoot(document.getElementById('root')).render(
           </ConfirmProvider>
         </ToastProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
